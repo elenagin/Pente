c41ad6408e6d48a9586223f99611116c84267689
Lenn <lennginzan@gmail.com>
2018-04-22 20:29:25 -0500

    principios de menu principal

--- tablero.c
+++ tablero.c
@@ -2,23 +2,30 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
-void StopTheApp(GtkWidget *window, gpointer data);
+void StopTheApp(GtkWidget *ventana, gpointer data);
 
 gint main ( gint argc, gchar *argv[])
 {
   
-  GtkWidget *window;
+  GtkWidget *ventana;
+  GtkWidget *MenuPrincipal;
   
   gtk_init(&argc, &argv);
   
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL); //crear ventana de texto
-  gtk_window_set_title(GTK_WINDOW (window), "PENTE");
-  gtk_window_set_default_size(GTK_WINDOW(window),500,500);
-  gtk_container_border_width(GTK_CONTAINER(window),5);
+  //crear ventana de texto
+  ventana = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW (ventana), "PENTE");
+  gtk_window_set_default_size(GTK_WINDOW(ventana),500,500);
+  gtk_container_border_width(GTK_CONTAINER(ventana),5);
+
+  //crear menu princicpal
+  MenuPrincipal = gtk_menu_bar_new ();
   
-  gtk_signal_connect(GTK_OBJECT(window),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
+  gtk_signal_connect(GTK_OBJECT(ventana),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
+  gtk_signal_connect(GTK_OBJECT(MenuPrincipal),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
   
-  gtk_widget_show_all(window);
+  gtk_widget_show_all(ventana);
+  gtk_widget_show_all(MenuPrincipal);
   
   gtk_main();
 
@@ -26,7 +33,7 @@ gint main ( gint argc, gchar *argv[])
 
 }
 
-void StopTheApp(GtkWidget *window, gpointer data)
+void StopTheApp(GtkWidget *ventana, gpointer data)
 {
     gtk_main_quit();
 }
