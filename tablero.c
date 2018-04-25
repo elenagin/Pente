b3fac3d6cbbde3b564e6108f43150c8fee332cbf
Elena Ginebra <32081397+lennygin@users.noreply.github.com>
2018-04-24 19:43:56 -0500

    agregar imagen de ventana de inicio

--- tablero.c
+++ tablero.c
@@ -7,25 +7,28 @@ void StopTheApp(GtkWidget *ventana, gpointer data);
 gint main ( gint argc, gchar *argv[])
 {
   
-  GtkWidget *ventana;
-  GtkWidget *MenuPrincipal;
+  GtkWidget *ventana, *imagen;
+  GdkPixbuf *pixbuf;
   
   gtk_init(&argc, &argv);
   
-  //crear ventana de texto
   ventana = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW (ventana), "PENTE");
-  gtk_window_set_default_size(GTK_WINDOW(ventana),500,500);
+  gtk_window_set_position(GTK_WINDOW(ventana), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(ventana),320,320);
   gtk_container_border_width(GTK_CONTAINER(ventana),5);
 
-  //crear menu princicpal
-  MenuPrincipal = gtk_menu_bar_new ();
+  pixbuf = gdk_pixbuf_new_from_file_at_scale("logo.png", 500, 500, FALSE, NULL);
+  imagen = gtk_image_new_from_pixbuf(pixbuf);
+
   
+  gtk_container_add (GTK_CONTAINER (ventana), imagen);
+  gtk_widget_show (imagen);
+			       
   gtk_signal_connect(GTK_OBJECT(ventana),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
-  gtk_signal_connect(GTK_OBJECT(MenuPrincipal),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
   
   gtk_widget_show_all(ventana);
-  gtk_widget_show_all(MenuPrincipal);
+
   
   gtk_main();
 
