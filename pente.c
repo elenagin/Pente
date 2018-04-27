e65aa050fcc2873762b5610594a4b17ad73f07ab
Lenn <lennginzan@gmail.com>
2018-04-26 20:05:00 -0500

    inicio de modulo ventana logo

--- pente.c
+++ pente.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <string.h>
+#include "try.h"
 
-int main (void)
+gint main (gint argc, gchar **argv)
 {
+
+
+  /*1.INICIALIZAR EL AMBIENTE*/
+  gtk_init(&argc, &argv);
+
+  VentanaLogo(&argc, &argv);
+
+  gtk_main();
+  return 0;
 }
