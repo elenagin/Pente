bcb96e1ce72fde6614b3e694cc92f9a53732b57e
Lenn <lennginzan@gmail.com>
2018-05-09 14:39:12 -0500

    1eras ventanas funcionando

---
+++ funciones.h
@@ -0,0 +1,5 @@
+void VentanaLogo (gint *argc, gchar ***argv);
+void StopTheApp(GtkWidget *ventana, gpointer data);
+void Hide(GtkWidget *imagen, gpointer data);
+void Hide2(GtkWidget *imagen, gpointer data);
+void VentanaInstrucciones(GtkWidget *widget, gpointer data);
