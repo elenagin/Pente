e6c108ef9a3abe71783fcfcf0720545a82fc7994
Lenn <lennginzan@gmail.com>
2018-05-09 14:37:44 -0500

    1eras ventanas funcionando

--- ventanainstrucciones.c
+++ ventanainstrucciones.c
@@ -1,15 +1,16 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include "tipos.h"
+#include "funciones.h"
 
-
-void VentanaInstrucciones(GtkWidget *ventana, gpointer data)
+void VentanaInstrucciones(GtkWidget *widget, gpointer data)
 { 
-  GtkWidget *imagen, *boton;
+  GtkWidget *imagen, *boton, *hbox;
   GdkPixbuf *pixbuf;
   typeVentana *ventanas=(typeVentana*)data;
-
-  //gtk_widget_hide_all(ventanas->VentanaLogo);
+  
+  gtk_widget_hide(ventanas->VentanaLogo);
+  hbox= gtk_hbox_new (TRUE, 5);
   ventanas->VentanaInstrucciones = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW (ventanas->VentanaInstrucciones), "Pente");
   gtk_window_set_position(GTK_WINDOW(ventanas->VentanaInstrucciones), GTK_WIN_POS_CENTER);
@@ -23,14 +24,11 @@ void VentanaInstrucciones(GtkWidget *ventana, gpointer data)
   gtk_widget_show (imagen);
 
   gtk_signal_connect(GTK_OBJECT(boton),"clicked",GTK_SIGNAL_FUNC(Hide), NULL);
+  gtk_signal_connect(GTK_OBJECT(ventanas->VentanaInstrucciones),"destroy",GTK_SIGNAL_FUNC(StopTheApp),NULL);
+  
   gtk_container_add(GTK_CONTAINER(ventanas->VentanaInstrucciones), boton);
-  gtk_container_add(GTK_CONTAINER(boton), imagen);
-
+  gtk_container_add(GTK_CONTAINER(boton), hbox);
+  gtk_container_add(GTK_CONTAINER(hbox), GTK_WIDGET(imagen));
+  
   gtk_widget_show_all(ventanas->VentanaInstrucciones); 
-}
-
-
-void Hide (GtkWidget *imagen, gpointer data)
-{
-  gtk_widget_hide(imagen);
 }
