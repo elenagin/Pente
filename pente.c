b7b2ba56afda5c1d0aaf4621e7660698ec44c80f
Elena Ginebra <32081397+lennygin@users.noreply.github.com>
2018-05-09 19:44:36 -0500

    Update pente.c

--- pente.c
+++ pente.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
-#include <stdlib.h>
-#include <string.h>
-#include "try.h"
+
+#include "tipos.h"
+#include "funciones.h"
 
 gint main (gint argc, gchar **argv)
 {
-
-
-  /*1.INICIALIZAR EL AMBIENTE*/
   gtk_init(&argc, &argv);
-
+  
   VentanaLogo(&argc, &argv);
 
   gtk_main();
