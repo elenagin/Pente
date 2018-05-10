d8043457e6c9f6040e090661c63a159f229e54a8
Elena Ginebra <32081397+lennygin@users.noreply.github.com>
2018-05-09 19:46:34 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,5 +1,5 @@
 void VentanaLogo (gint *argc, gchar ***argv);
 void StopTheApp(GtkWidget *ventana, gpointer data);
 void Hide(GtkWidget *imagen, gpointer data);
-void Hide2(GtkWidget *imagen, gpointer data);
 void VentanaInstrucciones(GtkWidget *widget, gpointer data);
+void VentanaNuevoAnterior(GtkWidget *widget, gpointer data);
