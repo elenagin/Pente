747ddd88fce8fe1727cf4186380e1cefcc3268ad
karinaalmazan <38571315+karinaalmazan@users.noreply.github.com>
2018-05-10 16:40:16 -0500

    Update tipos.h

--- Proyecto/ParteGrafica/tipos.h
+++ Proyecto/ParteGrafica/tipos.h
@@ -1,19 +1,18 @@
 /*******Estructuras a utilizar*************/
 
-
 struct def_Ventanas
 {
-  GtkWidget *VenP;
-  GtkWidget *M1;
-  GtkWidget *M2;
-  GtkWidget *M3;
-  GtkWidget *Des1;
-  GtkWidget *Des2;
-  GtkWidget *VenJ;
-  GtkWidget *VenA;
-  GtkWidget *VenG;
-  GtkWidget *VenAy;
-  GtkWidget *VenAd;
+  GtkWidget *VenP;//Ventana Principal
+  GtkWidget *M1;//Mensaje 1
+  GtkWidget *M2;//Mensaje 2
+  GtkWidget *M3;//Mensaje 3
+  GtkWidget *Des1;//Dialogo 1
+  GtkWidget *Des2;//Dialogo 2
+  GtkWidget *VenJ;//Ventana Jugar
+  GtkWidget *VenA;//Ventana Abrir
+  GtkWidget *VenG;//Ventana Guardar
+  GtkWidget *VenAy;//Ventana Ayuda
+  GtkWidget *VenAd;//Ventana Acerca de
 };
 
 typedef struct def_Ventanas tVentanas;
@@ -21,19 +20,16 @@ typedef struct def_Ventanas *ptrVentanas;
 
 struct def_Tablero
 {
-  GtkWidget *B[20][20];
-  GtkWidget *Im[20][20];
-  int Estados[20][20];
-  int Activo;
-  int Turno;
-  int Inicia;
-  int Min;
-  int Seg;
-  GtkWidget *NComidas;
-  GtkWidget *BComidas;
-  GtkWidget *EJ[2];
-  GtkWidget *Reloj;
-  FILE *Temporal;
+  GtkWidget *B[20][20];//Botones
+  GtkWidget *Im[20][20]; //Botones Imagenes(Al boton se le agrega la imagen)
+  int Estados[20][20];//0-Vacio,1-Juagador1, 2-Jugador2
+  int Activo;//Partida Activa(Bandera)
+  int Turno;//De quien es el turno
+  int Inicia;//Que jugador inicia
+  GtkWidget *NComidas;//Rojas comidas
+  GtkWidget *BComidas;//Azules comidas
+  GtkWidget *EJ[2];//Nombres de los jugadores
+  FILE *Temporal;//Archivo
 
 };
 
@@ -42,21 +38,21 @@ typedef struct def_Tablero *ptrTablero;
 
 struct def_Opciones
 {
-  GtkWidget *CHJ[3];
-  GSList *Grupo;
-  GtkWidget *Entry[2];
-  const gchar *Jugadores[2];
-  GtkWidget *BotonInicia[2];
+  GtkWidget *CHJ[3];//Caja Horizontal del Jugador
+  GSList *Grupo;//Grupo de los radiobuttons
+  GtkWidget *Entry[2];//entries de los nobres de jugadores
+  const gchar *Jugadores[2];//Lugar donde se guarda el nombre del jugador
+  GtkWidget *BotonInicia[2];//Radiobuttons
 };
 
 typedef struct def_Opciones tOpciones;
 typedef struct def_Opciones *ptrOpciones;
 
-struct def_Widgets
+struct def_Widgets//Estructura donde se incluyen las 3 estructuras
 {
-  ptrTablero Nodo;
-  ptrOpciones Nodo2;
-  ptrVentanas Nodo3;
+  ptrTablero SVentanas;
+  ptrOpciones STablero;
+  ptrVentanas SOpciones;
 };
 
 typedef struct def_Widgets tWidgets;
