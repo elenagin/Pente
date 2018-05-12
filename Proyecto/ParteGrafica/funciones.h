88309983eab5036bd58d4e6727ff16e47dcd8dae
Lenn <lennginzan@gmail.com>
2018-05-12 17:11:19 -0500

    agregar funciones de ventanas

--- Proyecto/ParteGrafica/funciones.h
+++ Proyecto/ParteGrafica/funciones.h
@@ -1,3 +1,9 @@
+/*Funciones Prototipo*/
+void VentanaPrincipal(GtkWidget *widget, gpointer data);
+void VentanaJuego(ptrWidgets Widgets);
+void VentanaBienvenida(ptrWidgets Widgets);
+void VentanaInstrucciones(GtkWidget *widget, gpointer data);
+
 void CargarPartida(ptrWidgets Widgets, char *NombreArchivo);
 void GuardarPartida(ptrWidgets Widgets, char *NombreArchivo);
 gboolean Juez(GtkWidget *Widget, ptrWidgets Widgets);
