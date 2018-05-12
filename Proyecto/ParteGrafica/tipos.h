fdfb7cbd57c5d78ec35ac1f12cbab70f1316ef72
karinaalmazan <38571315+karinaalmazan@users.noreply.github.com>
2018-05-12 10:07:46 -0500

    Update tipos.h

--- Proyecto/ParteGrafica/tipos.h
+++ Proyecto/ParteGrafica/tipos.h
@@ -1,62 +1,80 @@
-/*******Estructuras a utilizar*************/
+/*******************************************
+*  @file tipos.h                           *
+*                                          *
+*  @brief Este archivo incluye todas las   *
+*         estructuras a utilizar en la     *
+*         desarrollo de la parte grafica   *
+*         de este proyecto.                *
+*  @date 24/04/2018
+*******************************************/
 
-
+/*******************************************
+*   Estructura en donde se inluyen todos   *
+*   los elementos a ocupar en las ventanas *
+*******************************************/
 struct def_Ventanas
 {
-  GtkWidget *VenP;
-  GtkWidget *M1;
-  GtkWidget *M2;
-  GtkWidget *M3;
-  GtkWidget *Des1;
-  GtkWidget *Des2;
-  GtkWidget *VenJ;
-  GtkWidget *VenA;
-  GtkWidget *VenG;
-  GtkWidget *VenAy;
-  GtkWidget *VenAd;
+  GtkWidget *VenP;//Ventana Principal
+  GtkWidget *M1;//Mensaje 1
+  GtkWidget *M2;//Mensaje 2
+  GtkWidget *M3;//Mensaje 3
+  GtkWidget *Des1;//Dialogo 1
+  GtkWidget *Des2;//Dialogo 2
+  GtkWidget *VenJ;//Ventana jugar
+  GtkWidget *VenA;//Ventana Abrir
+  GtkWidget *VenG;//Ventana GUardar
+  GtkWidget *VenAy;//Ventana Ayuda
+  GtkWidget *VenAd;//Ventana Acerca de 
 };
 
 typedef struct def_Ventanas tVentanas;
 typedef struct def_Ventanas *ptrVentanas;
 
+/*******************************************
+*   Estructura en donde se inluyen todos   *
+*   los elementos a ocupar en el tablero   *
+*******************************************/
 struct def_Tablero
 {
-  GtkWidget *B[20][20];
-  GtkWidget *Im[20][20];
-  int Estados[20][20];
-  int Activo;
-  int Turno;
-  int Inicia;
-  int Min;
-  int Seg;
-  GtkWidget *NComidas;
-  GtkWidget *BComidas;
-  GtkWidget *EJ[2];
-  GtkWidget *Reloj;
-  FILE *Temporal;
-
+  GtkWidget *B[20][20];//Botones
+  GtkWidget *Im[20][20];//Botones imagenes(al boton se le agrega la imagen)
+  int Estados[20][20];//0-Vacio,1-Jugador1, 2-Jugador2
+  int Activo;//Partida activa(bandera)
+  int Turno;//De quien es el turno 
+  int Inicia;//Que jugador inicia
+  GtkWidget *AComidas;//Azules comidas
+  GtkWidget *RComidas;//Rosas comidas
+  GtkWidget *EJ[2];//Nombres de los jugadores
+  FILE *Temporal;//Archivo
 };
 
 typedef struct def_Tablero tTablero;
 typedef struct def_Tablero *ptrTablero;
 
+/*******************************************
+*   Estructura en donde se inluyen todos   *
+*   los elementos a ocupar en las opciones *
+*******************************************/
 struct def_Opciones
 {
-  GtkWidget *CHJ[3];
-  GSList *Grupo;
-  GtkWidget *Entry[2];
-  const gchar *Jugadores[2];
-  GtkWidget *BotonInicia[2];
+  GtkWidget *CHJ[3];//Caja Horizontal del Jugador
+  GSList *Grupo;//Grupo de los radiobuttons
+  GtkWidget *Entry[2];//Entries de los nombres de jugadores
+  const gchar *Jugadores[2];//Lugar donde se guarda el nombre del jugador
+  GtkWidget *BotonInicia[2];//Radiobuttons
 };
 
 typedef struct def_Opciones tOpciones;
 typedef struct def_Opciones *ptrOpciones;
-
+/*******************************************
+*   Estructura en donde se inluyen las 3   *
+*        estructuras anteriores            *
+*******************************************/
 struct def_Widgets
 {
-  ptrTablero Nodo;
-  ptrOpciones Nodo2;
-  ptrVentanas Nodo3;
+  ptrTablero  STablero;
+  ptrOpciones SOpciones;
+  ptrVentanas SVentanas;
 };
 
 typedef struct def_Widgets tWidgets;
