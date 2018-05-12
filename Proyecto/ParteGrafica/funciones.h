b610e3835255540d972e16abc08b5b3aeaa4923d
Elena Ginebra <32081397+lennygin@users.noreply.github.com>
2018-05-11 23:49:44 -0500

    flashback

--- Proyecto/ParteGrafica/funciones.h
+++ Proyecto/ParteGrafica/funciones.h
@@ -4,9 +4,3 @@ gboolean Juez(GtkWidget *Widget, ptrWidgets Widgets);
 gboolean DialogoCerrar(ptrWidgets Widgets);
 gboolean Ganador(ptrWidgets Widgets, int a);
 int Heuristica(ptrTablero Nodo,int i,int j);
-
-void VentanaLogo (ptrWidgets Widgets);
-void StopTheApp(GtkWidget *ventana, gpointer data);
-void Hide(GtkWidget *imagen, gpointer data);
-void VentanaInstrucciones(GtkWidget *widget, gpointer data);
-void VentanaNuevoAnterior(GtkWidget *widget, gpointer data);
