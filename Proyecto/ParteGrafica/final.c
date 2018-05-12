b0b5698efec72197fe57cd9a97321e77c9a8c6d0
karinaalmazan <38571315+karinaalmazan@users.noreply.github.com>
2018-05-12 10:24:44 -0500

    Update final.c

--- Proyecto/ParteGrafica/final.c
+++ Proyecto/ParteGrafica/final.c
@@ -31,13 +31,13 @@ int main(int argc, char *argv[])
   /*Inicializamos el ambiente*/
   gtk_init(&argc,&argv);
   Widgets=(ptrWidgets)malloc(sizeof(tWidgets));/*Creamos nuestro paquete de Widgets*/
-  Widgets->Nodo3=(ptrVentanas)malloc(sizeof(tVentanas));
-  Widgets->Nodo=(ptrTablero)malloc(sizeof(tTablero));
-  Widgets->Nodo2=(ptrOpciones)malloc(sizeof(tOpciones));//creacion de widgets
+  Widgets->SVentanas=(ptrVentanas)malloc(sizeof(tVentanas));
+  Widgets->STablero=(ptrTablero)malloc(sizeof(tTablero));
+  Widgets->SOpciones=(ptrOpciones)malloc(sizeof(tOpciones));//creacion de widgets
   VentanaPrincipal(Widgets);//Se crea la ventana principal
   VentanaJuego(Widgets);//Se crea la ventana de nueva partida
   VentanaBienvenida(Widgets);//Se crea la ventana de presentacion y se espera a que se le de aceptar
-  gtk_widget_show_all(Widgets->Nodo3->VenP);//Mostramos todos los widgests incluidos en la ventana
+  gtk_widget_show_all(Widgets->SVentanas->VenP);//Mostramos todos los widgests incluidos en la ventana
   gtk_main();/*funcion inicio iterada*/
   
 }//main
@@ -72,14 +72,14 @@ void VentanaPrincipal(ptrWidgets Widgets)
   Tabla=gtk_table_new(20,20,TRUE);//Creamos tabla
 
   //Ventana
-  Widgets->Nodo3->VenP=gtk_window_new(GTK_WINDOW_TOPLEVEL);//Creamos ventana principal, esta se encuentra en una estructura
-  gtk_window_set_resizable(GTK_WINDOW(Widgets->Nodo3->VenP),FALSE);//fijamos atributo de la ventana 
-  gtk_window_set_position(GTK_WINDOW(Widgets->Nodo3->VenP),GTK_WIN_POS_CENTER);//fijamos posicion de la ventana
+  Widgets->SVentanas->VenP=gtk_window_new(GTK_WINDOW_TOPLEVEL);//Creamos ventana principal, esta se encuentra en una estructura
+  gtk_window_set_resizable(GTK_WINDOW(Widgets->SVentanas->VenP),FALSE);//fijamos atributo de la ventana 
+  gtk_window_set_position(GTK_WINDOW(Widgets->SVentanas->VenP),GTK_WIN_POS_CENTER);//fijamos posicion de la ventana
   //Registramos la llamada a funciones con g_signal_connect, usaremos la funcion salir
-  gtk_window_set_title(GTK_WINDOW (Widgets->Nodo3->VenP), "Pente");
-  g_signal_connect(Widgets->Nodo3->VenP,"delete_event",G_CALLBACK(CerrarJuego1), Widgets);
-  g_signal_connect(Widgets->Nodo3->VenP,"destroy",G_CALLBACK(CerrarJuego2), Widgets);
-  gtk_container_add(GTK_CONTAINER(Widgets->Nodo3->VenP),CV);//definimos jerarquia, quien contiene a quien
+  gtk_window_set_title(GTK_WINDOW (Widgets->SVentanas->VenP), "Pente");
+  g_signal_connect(Widgets->SVentanas->VenP,"delete_event",G_CALLBACK(CerrarJuego1), Widgets);
+  g_signal_connect(Widgets->SVentanas->VenP,"destroy",G_CALLBACK(CerrarJuego2), Widgets);
+  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenP),CV);//definimos jerarquia, quien contiene a quien
   
   //Barra de menu
   MenuB=gtk_menu_bar_new();//creamos barra de menu
@@ -99,7 +99,7 @@ void VentanaPrincipal(ptrWidgets Widgets)
   MenuIM=gtk_menu_item_new_with_mnemonic("_Manual");//el menu tiene el texto manual
   g_signal_connect(MenuIM,"activate",G_CALLBACK(Manual),Widgets);//llamada a la funcion Manual
   MenuIAd=gtk_menu_item_new_with_mnemonic("A_cerca de");//Creacion de Item  Acerca de
-  g_signal_connect(MenuIAd,"activate",G_CALLBACK(Acerca_de),Widgets->Nodo3);//llamada a la funcion acerca de
+  g_signal_connect(MenuIAd,"activate",G_CALLBACK(Acerca_de),Widgets->SVentanas);//llamada a la funcion acerca de
   
   /*Definimos jerarquia para poder meter los items en los menus*/
   gtk_menu_shell_append(GTK_MENU_SHELL(Menu1), MenuIJ);
@@ -127,7 +127,7 @@ void VentanaPrincipal(ptrWidgets Widgets)
   TAy=gtk_tool_button_new_from_stock(GTK_STOCK_HELP);//creacion del boton ayuda
   g_signal_connect(TAy,"clicked",G_CALLBACK(Manual),Widgets);//llamada a la funcion Manual
   TAd=gtk_tool_button_new_from_stock(GTK_STOCK_ABOUT);//creacion del icono acerca de
-  g_signal_connect(TAd,"clicked",G_CALLBACK(Acerca_de),Widgets->Nodo3);//llamada a la funcion Acerca de
+  g_signal_connect(TAd,"clicked",G_CALLBACK(Acerca_de),Widgets->SVentanas);//llamada a la funcion Acerca de
   Sep2=gtk_separator_tool_item_new();//creamos un segundo separador
   TS=gtk_tool_button_new_from_stock(GTK_STOCK_QUIT);//cracion del icono salir
   g_signal_connect(TS,"clicked",G_CALLBACK(CerrarJuego),Widgets);//llamada a la funcion Salir
@@ -147,23 +147,23 @@ void VentanaPrincipal(ptrWidgets Widgets)
   
   //Tablero de juego e inizializacion de datos
   gtk_box_pack_start_defaults (GTK_BOX(CV),CH);//Caja de tablero y cuadro
-  Widgets->Nodo->Activo=0;//Significa si hay una partida en juego
-  Widgets->Nodo->Turno=0;//Significa el turno correspondiente
+  Widgets->STablero->Activo=0;//Significa si hay una partida en juego
+  Widgets->STablero->Turno=0;//Significa el turno correspondiente
   Tabla=gtk_table_new(20,20,TRUE);//creacion de tabla
   for(i=0;i<20;i++)
     {
       for(j=0;j<20;j++)//inicializamos la imagen poniendola vacia
 	{
-	  Widgets->Nodo->Estados[i][j]=0;
-	  Widgets->Nodo->B[i][j]=gtk_button_new();//nuevo boton
-	  Widgets->Nodo->Im[i][j]=gtk_image_new();//nueva imagen
-	  gtk_button_set_image(GTK_BUTTON(Widgets->Nodo->B[i][j]),Widgets->Nodo->Im[i][j]);
-	  gtk_image_set_from_file(GTK_IMAGE(Widgets->Nodo->Im[i][j]),"Archivos/0.png");
+	  Widgets->STablero->Estados[i][j]=0;
+	  Widgets->STablero->B[i][j]=gtk_button_new();//nuevo boton
+	  Widgets->STablero->Im[i][j]=gtk_image_new();//nueva imagen
+	  gtk_button_set_image(GTK_BUTTON(Widgets->STablero->B[i][j]),Widgets->STablero->Im[i][j]);
+	  gtk_image_set_from_file(GTK_IMAGE(Widgets->STablero->Im[i][j]),"Archivos/0.png");
 	  sprintf(NombreBoton,"%d,%d",i,j);//modificar una cadena que sera nuestro nombre de boton
-	  gtk_widget_set_name(Widgets->Nodo->B[i][j],NombreBoton);//dar nombre al boton
-	  gtk_widget_modify_bg(Widgets->Nodo->B[i][j],GTK_STATE_NORMAL,&Tablero);      
-	  gtk_table_attach_defaults(GTK_TABLE(Tabla),Widgets->Nodo->B[i][j],i,i+1,j,j+1);
-	  g_signal_connect(Widgets->Nodo->B[i][j],"clicked",G_CALLBACK(Pulsado), Widgets);//llamada a la funcion dando click
+	  gtk_widget_set_name(Widgets->STablero->B[i][j],NombreBoton);//dar nombre al boton
+	  gtk_widget_modify_bg(Widgets->STablero->B[i][j],GTK_STATE_NORMAL,&Tablero);      
+	  gtk_table_attach_defaults(GTK_TABLE(Tabla),Widgets->STablero->B[i][j],i,i+1,j,j+1);
+	  g_signal_connect(Widgets->STablero->B[i][j],"clicked",G_CALLBACK(Pulsado), Widgets);//llamada a la funcion dando click
 	}//for
     }//for
   gtk_box_pack_start_defaults (GTK_BOX(CH),Tabla);//agregamos tabla a la caja vertical
@@ -174,21 +174,21 @@ void VentanaPrincipal(ptrWidgets Widgets)
 
   CHPartida[0]=gtk_hbox_new(FALSE,10);//creacion de caja horizontal
   gtk_box_pack_start_defaults (GTK_BOX(CPartida),CHPartida[0]);//caja horizontal en caja vertical
-  Widgets->Nodo->BComidas=gtk_label_new("00");//creacion de label que indicara las jugadas o comidas
-  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[0]),Widgets->Nodo->BComidas);//incluimos en la caja vertical el label
+  Widgets->STablero->RComidas=gtk_label_new("00");//creacion de label que indicara las jugadas o comidas
+  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[0]),Widgets->STablero->RComidas);//incluimos en la caja vertical el label
   Im1=gtk_image_new_from_file("Archivos/1.png");//creamos imagen
   gtk_box_pack_start_defaults (GTK_BOX(CHPartida[0]),Im1);//agregamos imagen en la caja horizontal
-  Widgets->Nodo->EJ[0]=gtk_label_new("Jugador1");//creamos label
-  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[0]),Widgets->Nodo->EJ[0]);//agregamos label en caja horizontal
+  Widgets->STablero->EJ[0]=gtk_label_new("Jugador1");//creamos label
+  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[0]),Widgets->STablero->EJ[0]);//agregamos label en caja horizontal
 
   CHPartida[1]=gtk_hbox_new(FALSE,10);//creacion de segunda caja horizontal
   gtk_box_pack_start_defaults (GTK_BOX(CPartida),CHPartida[1]);//agregamos caja horizontal en caja de partida
-  Widgets->Nodo->NComidas=gtk_label_new("00");//jugadas del jugador 2
-  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[1]),Widgets->Nodo->NComidas);//agregamos comidas a la caja horizontal
+  Widgets->STablero->AComidas=gtk_label_new("00");//jugadas del jugador 2
+  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[1]),Widgets->STablero->AComidas);//agregamos comidas a la caja horizontal
   Im2=gtk_image_new_from_file("Archivos/2.png");//creamos imagen para jugador 2
   gtk_box_pack_start_defaults (GTK_BOX(CHPartida[1]),Im2);//agregamos imagen a la caja horizontal
-  Widgets->Nodo->EJ[1]=gtk_label_new("Jugador2");//creamos label
-  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[1]),Widgets->Nodo->EJ[1]);//agregamos label en la caja horizontal
+  Widgets->STablero->EJ[1]=gtk_label_new("Jugador2");//creamos label
+  gtk_box_pack_start_defaults (GTK_BOX(CHPartida[1]),Widgets->STablero->EJ[1]);//agregamos label en la caja horizontal
 
   CHPartida[2]=gtk_fixed_new();//centramos el tiempo
   gtk_box_pack_start_defaults (GTK_BOX(CPartida),CHPartida[2]);//incluimos la caja de tiempo en la caja de partida
@@ -216,39 +216,39 @@ void VentanaJuego(ptrWidgets Widgets)
   //Cajas
   CVJ=gtk_vbox_new(FALSE,5);//nueva caja vertical
   for(i=0;i<4;i++)//creacion de cajas horizontales
-    Widgets->Nodo2->CHJ[i]=gtk_hbox_new(FALSE,3);
+    Widgets->SOpciones->CHJ[i]=gtk_hbox_new(FALSE,3);
 
   //Ventana
-  Widgets->Nodo3->VenJ=gtk_window_new(GTK_WINDOW_TOPLEVEL);//creacion de ventana principal
-  gtk_window_set_position(GTK_WINDOW(Widgets->Nodo3->VenJ),GTK_WIN_POS_CENTER);//definicion de la posicion
-  gtk_window_set_title(GTK_WINDOW(Widgets->Nodo3->VenJ),"Modo de juego");//damos nombre a la ventana
-  gtk_container_add(GTK_CONTAINER(Widgets->Nodo3->VenJ),CVJ);//agregamos la caja vertical de jugador a la ventana
-  gtk_window_set_resizable(GTK_WINDOW(Widgets->Nodo3->VenJ),FALSE);//definimos tamaño de ventana
+  Widgets->SVentanas->VenJ=gtk_window_new(GTK_WINDOW_TOPLEVEL);//creacion de ventana principal
+  gtk_window_set_position(GTK_WINDOW(Widgets->SVentanas->VenJ),GTK_WIN_POS_CENTER);//definicion de la posicion
+  gtk_window_set_title(GTK_WINDOW(Widgets->SVentanas->VenJ),"Modo de juego");//damos nombre a la ventana
+  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenJ),CVJ);//agregamos la caja vertical de jugador a la ventana
+  gtk_window_set_resizable(GTK_WINDOW(Widgets->SVentanas->VenJ),FALSE);//definimos tamaño de ventana
   
   //Nombre Jugador1
-  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->Nodo2->CHJ[0]);
+  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->SOpciones->CHJ[0]);
   EtiquetasJ[1]=gtk_label_new("Jugador1:");//nueva etiqueta
-  Widgets->Nodo2->Entry[0]=gtk_entry_new();//nuevo entry
-  gtk_entry_set_max_length(GTK_ENTRY(Widgets->Nodo2->Entry[0]),30);//tamaño de entry
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[0]),EtiquetasJ[1]);//agregamos etiqueta a caja horizontal
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[0]),Widgets->Nodo2->Entry[0]);//agregamos entry a caja horizontal
+  Widgets->SOpciones->Entry[0]=gtk_entry_new();//nuevo entry
+  gtk_entry_set_max_length(GTK_ENTRY(Widgets->SOpciones->Entry[0]),30);//tamaño de entry
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[0]),EtiquetasJ[1]);//agregamos etiqueta a caja horizontal
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[0]),Widgets->SOpciones->Entry[0]);//agregamos entry a caja horizontal
 
   //Nombre Jugador2
-  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->Nodo2->CHJ[1]);
+  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->SOpciones->CHJ[1]);
   EtiquetasJ[2]=gtk_label_new("Jugador2");//nueva etiqueta
-  Widgets->Nodo2->Entry[1]=gtk_entry_new();//nuevo entry
-  gtk_entry_set_max_length(GTK_ENTRY(Widgets->Nodo2->Entry[1]),30);//tamaño de entry
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[1]),EtiquetasJ[2]);//agregamos etiqueta a caja horizontal
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[1]),Widgets->Nodo2->Entry[1]);//agregamos entry a caja horizontal
+  Widgets->SOpciones->Entry[1]=gtk_entry_new();//nuevo entry
+  gtk_entry_set_max_length(GTK_ENTRY(Widgets->SOpciones->Entry[1]),30);//tamaño de entry
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[1]),EtiquetasJ[2]);//agregamos etiqueta a caja horizontal
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[1]),Widgets->SOpciones->Entry[1]);//agregamos entry a caja horizontal
   
   //Orden de tiro
   EtiquetasJ[3]=gtk_label_new("¿Quien empieza el juego?");//nuevo label
-  Widgets->Nodo2->BotonInicia[0]=gtk_radio_button_new_with_label(NULL,"Jugador 1");//nuevo boton radio
-  Widgets->Nodo2->Grupo=gtk_radio_button_get_group(GTK_RADIO_BUTTON(Widgets->Nodo2->BotonInicia[0]));//definimos orden de boton
-  Widgets->Nodo2->BotonInicia[1]=gtk_radio_button_new_with_label(Widgets->Nodo2->Grupo,"Jugador 2");//agregamos nombre a boton
+  Widgets->SOpciones->BotonInicia[0]=gtk_radio_button_new_with_label(NULL,"Jugador 1");//nuevo boton radio
+  Widgets->SOpciones->Grupo=gtk_radio_button_get_group(GTK_RADIO_BUTTON(Widgets->SOpciones->BotonInicia[0]));//definimos orden de boton
+  Widgets->SOpciones->BotonInicia[1]=gtk_radio_button_new_with_label(Widgets->SOpciones->Grupo,"Jugador 2");//agregamos nombre a boton
   gtk_box_pack_start_defaults (GTK_BOX(CVJ),EtiquetasJ[3]);//agregamos label a caja vertical
-  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->Nodo2->BotonInicia[0]);//agregamos bton iniciala caja vertical
-  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->Nodo2->BotonInicia[1]);//agregamos los radios boton a la caja vertical
+  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->SOpciones->BotonInicia[0]);//agregamos bton iniciala caja vertical
+  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->SOpciones->BotonInicia[1]);//agregamos los radios boton a la caja vertical
 
   //Botones
   BotonJOK=gtk_button_new_from_stock(GTK_STOCK_OK);//nuevo boton OK
@@ -256,9 +256,9 @@ void VentanaJuego(ptrWidgets Widgets)
   BotonJCancel=gtk_button_new_from_stock(GTK_STOCK_CANCEL);//nuevo boton CANCEL
   g_signal_connect(BotonJCancel,"clicked",G_CALLBACK(Esconder),Widgets);//llamada a la funcion esconder
   
-  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->Nodo2->CHJ[2]);//agregamos caja horizontal a caja verticalJ
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[2]),BotonJOK);//boton OK  a caja horizontal 
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->Nodo2->CHJ[2]),BotonJCancel);//boton cancel a caja horizontal
+  gtk_box_pack_start_defaults (GTK_BOX(CVJ),Widgets->SOpciones->CHJ[2]);//agregamos caja horizontal a caja verticalJ
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[2]),BotonJOK);//boton OK  a caja horizontal 
+  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CHJ[2]),BotonJCancel);//boton cancel a caja horizontal
 }//VentanaJuego
 
 
@@ -273,13 +273,13 @@ void VentanaJuego(ptrWidgets Widgets)
  ****************************************************/
 void VentanaBienvenida(ptrWidgets Widgets)
 {
-  Widgets->Nodo3->M1=gtk_message_dialog_new(GTK_WINDOW(Widgets->Nodo3->VenP),
+  Widgets->SVentanas->M1=gtk_message_dialog_new(GTK_WINDOW(Widgets->SVentanas->VenP),
 					    GTK_DIALOG_DESTROY_WITH_PARENT,
 					    GTK_MESSAGE_OTHER,
 					    GTK_BUTTONS_OK,
 					    "Bienvenido jugador:\nEste programa fue elaborado por:\nMariana Martinez Kobeh\nElena Ginebra\nKarina Almazán");//nuevo mensaje de Dialogo, en este mismo esta ya incluido en el mensaje de dialogo los botones
-  gtk_window_set_position(GTK_WINDOW(Widgets->Nodo3->M1),GTK_WIN_POS_CENTER);//establecer posicion al centro
-  gtk_window_set_title(GTK_WINDOW(Widgets->Nodo3->M1), "Bienvenido");//nombre de la ventana
-  gtk_dialog_run(GTK_DIALOG(Widgets->Nodo3->M1));//correr el cuadro de dialogo
-  gtk_widget_destroy(Widgets->Nodo3->M1);//destruir cuadro de dialogo
+  gtk_window_set_position(GTK_WINDOW(Widgets->SVentanas->M1),GTK_WIN_POS_CENTER);//establecer posicion al centro
+  gtk_window_set_title(GTK_WINDOW(Widgets->SVentanas->M1), "Bienvenido");//nombre de la ventana
+  gtk_dialog_run(GTK_DIALOG(Widgets->SVentanas->M1));//correr el cuadro de dialogo
+  gtk_widget_destroy(Widgets->SVentanas->M1);//destruir cuadro de dialogo
 }//VentanaBienvenida
