507e5422fab9b5a44e23f783954898769174ab31
marianamkobeh <38571308+marianamkobeh@users.noreply.github.com>
2018-05-15 09:02:16 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,6 +1,24 @@
+/*
+ *@funciones.h
+ *Este programa es el .h de funciones.c ya que son módulos.
+ *Esto significa que incluye todas las funciones que no son static de
+ *funciones.h
+ *Se hizo lo de indef y endif (es un if) para que si ya se declaró esta
+ *biblioteca, no se vuelva a declarar y haya problemas en el código
+ *@Karina
+ *@Elena
+ *@01/05/2018
+**/
+
+#indef funcionesDEFINIDO
+
 void CargarPartida(ptrWidgets Widgets, char *NombreArchivo);
 void GuardarPartida(ptrWidgets Widgets, char *NombreArchivo);
 gboolean Juez(GtkWidget *Widget, ptrWidgets Widgets);
 gboolean DialogoCerrar(ptrWidgets Widgets);
 gboolean Ganador(ptrWidgets Widgets, int a);
 int Heuristica(ptrTablero Nodo,int i,int j);
+
+#define funcionesDEFINIDO
+#endif
+
